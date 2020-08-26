2a1f8a437040a77c2b47d07799a0bdc36f47ed11
Daniel.Yu <adam741963@gmail.com>
2020-08-26 12:18:04 -0700

    servers

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./Develop/public/routes/noteApiRoutes")(app);
-require("./Develop/public/routes/htmlRoutes")(app);
+require("./public/routes/noteApiRoutes")(app);
+require("./public/routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("PORT: " + PORT);
